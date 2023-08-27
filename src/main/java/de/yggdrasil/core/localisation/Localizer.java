7951f220ea1827521e91cb68a37f499c432588ff
Invincible2211 <fynn.schur@web.de>
2023-08-27 18:08:23 +0200

    initial commit

---
+++ src/main/java/de/yggdrasil/core/localisation/Localizer.java
@@ -0,0 +1,15 @@
+package de.yggdrasil.core.localisation;
+
+import de.yggdrasil.core.localisation.sesseion.LocalizerSession;
+
+public class Localizer {
+
+    public String localize (String identifier, Player target){
+
+    }
+
+    public LocalizerSession createSession(Player player){
+
+    }
+
+}
---
+++ src/main/java/de/yggdrasil/core/localisation/sesseion/LocalizerSession.java
@@ -0,0 +1,13 @@
+package de.yggdrasil.core.localisation.sesseion;
+
+import java.util.UUID;
+
+public class LocalizerSession {
+
+    private final UUID targetUUID;
+
+    public LocalizerSession(Player target){
+        targetUUID = traget.getUUID();
+    }
+
+}
